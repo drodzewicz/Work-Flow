2c2b33b42ce62a5ee01c4b9d877bf61500a5b1f7
DaRoTP <DRodzewicz@gmail.com>
2020-08-08 11:48:36 +0300

    NavBar blind spot fix in home button & Footer pixel perfect fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,11 +6,11 @@ import { LoginForm, RegisterForm } from "modalForms";
 import "./App.scss";
 import { ModalContext } from "context/ModalContext";
 import { UserContext } from "context/UserContext";
-import { BrowserRouter as Router, Link } from "react-router-dom";
-import AccountBoxIcon from '@material-ui/icons/AccountBox';
-import HomeIcon from '@material-ui/icons/Home';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import Badge from '@material-ui/core/Badge';
+import { useHistory, Link } from "react-router-dom";
+import AccountBoxIcon from "@material-ui/icons/AccountBox";
+import HomeIcon from "@material-ui/icons/Home";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Badge from "@material-ui/core/Badge";
 import Routes from "routes/Routes";
 import NavItem from "components/Navbar/NavItem";
 import SwitchButton from "components/SwitchButton/SwitchButton";
@@ -18,48 +18,78 @@ import Notification from "components/Notification/Notification";
 import Footer from "components/Footer/Footer";
 
 function App() {
-
   const [, modalDispatch] = useContext(ModalContext);
   const [user, setUser] = useContext(UserContext);
-  const [notifications, setNotification ] = useState([
-    {board: "wix websiite", message: "you have been added to the board"},
-    {board: "learing wordpress with friends", message: "you have been added to the boardyou have been addeddwd dwdwd dwdwd"},
-    {board: "making apython game", message: "you got a new task"},
+  const history = useHistory();
+  const [notifications, setNotification] = useState([
+    { board: "wix websiite", message: "you have been added to the board" },
+    {
+      board: "learing wordpress with friends",
+      message:
+        "you have been added to the boardyou have been addeddwd dwdwd dwdwd",
+    },
+    { board: "making apython game", message: "you got a new task" },
   ]);
 
   const openLoginModal = () => {
-    modalDispatch({type: "OPEN", payload: { render: <LoginForm />, title: "Login"}});
-  }
+    modalDispatch({
+      type: "OPEN",
+      payload: { render: <LoginForm />, title: "Login" },
+    });
+  };
   const openRegisterModal = () => {
-    modalDispatch({type: "OPEN", payload: { render: <RegisterForm />, title: "Register"}});
-  }
+    modalDispatch({
+      type: "OPEN",
+      payload: { render: <RegisterForm />, title: "Register" },
+    });
+  };
   const logOutUser = () => {
     console.log("log out");
     setUser({ ...user, username: null });
-  }
+  };
   const removeMessage = (index) => {
     let tempNotification = [...notifications];
-    tempNotification.splice(index, 1)
+    tempNotification.splice(index, 1);
     setNotification(tempNotification);
-    
-  }
+  };
+  const goToHomePage = () => {
+    history.push("/");
+  };
 
   const loggedInUserNavItems = () => {
     return (
       <>
-        <NavItem icon={<Link to="/"><HomeIcon /></Link>} />
-        <NavItem icon={<AccountBoxIcon />} navName={user.username} classes={["profile-nav"]}>
+        <NavItem clicked={goToHomePage} icon={<HomeIcon />} />
+        <NavItem
+          icon={<AccountBoxIcon />}
+          navName={user.username}
+          classes={["profile-nav"]}
+        >
           <Link to="/profile">Profile</Link>
-          <button className="logout-btn" onClick={logOutUser}>logout</button>
+          <button className="logout-btn" onClick={logOutUser}>
+            logout
+          </button>
           <SwitchButton />
         </NavItem>
-        <NavItem  classes={["notification-nav"]} icon={
-          <Badge color="secondary" variant="dot" invisible={notifications.length < 1} >
-            <NotificationsIcon />
-          </Badge>
-        } >
+        <NavItem
+          classes={["notification-nav"]}
+          icon={
+            <Badge
+              color="secondary"
+              variant="dot"
+              invisible={notifications.length < 1}
+            >
+              <NotificationsIcon />
+            </Badge>
+          }
+        >
           {notifications.map((data, index) => (
-            <Notification key={data.board} message={data.message} boardTitle={data.board} removeNotification={() => removeMessage(index)} />
+            <Notification
+              key={data.board}
+              message={data.message}
+              boardTitle={data.board}
+              removeNotification={() => removeMessage(index)}
+            />
           ))}
         </NavItem>
       </>
@@ -76,19 +106,13 @@ function App() {
 
   return (
     <div className="App">
-      <Router>
-        <Modal />
-        <Navbar>
-          {
-            user.username
-              ? loggedInUserNavItems()
-              : loggedOutUserNavItems()
-          }
-        </Navbar>
-        <ContainerBox>
-          <Routes />
-        </ContainerBox>
-      </Router>
+      <Modal />
+      <Navbar>
+        {user.username ? loggedInUserNavItems() : loggedOutUserNavItems()}
+      </Navbar>
+      <ContainerBox>
+        <Routes />
+      </ContainerBox>
       <Footer />
     </div>
   );
