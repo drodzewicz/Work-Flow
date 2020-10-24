3123f21fcdd71dd498e4dde7bfb20c2fb707426f
DaRoBB <drodzewicz@gmail.com>
2020-10-25 01:22:14 +0200

    multistage registartion form - seperated registration into two stages in order to shrink the form, first stage is credentials name and surname and second/last is email username password

--- client/src/modalForms/Register/Register.js
+++ client/src/modalForms/Register/Register.js
@@ -1,65 +1,37 @@
-import React, { useContext, useEffect } from "react";
-import * as Yup from "yup";
-import { ModalContext } from "context/ModalContext";
+import React, { useState } from "react";
 import "./Register.scss";
-import fetchData from "helper/fetchData";
-import SimpleForm from "components/SimpleForm/SimpleForm";
-import { Login } from "modalForms";
-
-const validationSchema = Yup.object({
-	username: Yup.string().max(25, "username is too long").required("field is required"),
-	password: Yup.string().min(5, "must be at least 5 characters").required("field is required"),
-	matchPassword: Yup.string()
-		.oneOf([Yup.ref("password")], "password does not match")
-		.required("Password confirm is required"),
-	email: Yup.string().email().required("field is required"),
-	name: Yup.string().max(25, "name is too long").required("field is required"),
-	surname: Yup.string().max(25, "surname is too long").required("field is required"),
-});
-
-const fields = {
-	username: { initialVal: "", type: "text" },
-	password: { initialVal: "", type: "password" },
-	matchPassword: { initialVal: "", type: "password", label: "match password" },
-	email: { initialVal: "", type: "email" },
-	name: { initialVal: "", type: "name" },
-	surname: { initialVal: "", type: "surname" },
-};
+import RegisterStage1 from "./RegisterStage1";
+import RegisterStage2 from "./RegisterStage2";
 
 const Register = () => {
-	const [, modalDispatch] = useContext(ModalContext);
-	let openLoginAfterRegisterTimeOut = null;
+	const [registerStage, setRegisterStage] = useState(1);
 
-	useEffect(() => {
-		return () => {
-			clearTimeout(openLoginAfterRegisterTimeOut);
-		};
-	}, [openLoginAfterRegisterTimeOut]);
+	const [registerFormField, setRegisterFormFields] = useState({
+		username: "",
+		password: "",
+		matchPassword: "",
+		email: "",
+		name: "",
+		surname: ""
+	})
 
-	const handleSubmit = async (submittedData, { setErrors, setSubmitting }) => {
-		const { data, error } = await fetchData({
-			method: "POST",
-			url: "/register",
-			setLoading: setSubmitting,
-			payload: submittedData,
-		});
-		if (!!data) {
-			modalDispatch({ type: "CLOSE" });
-			openLoginAfterRegisterTimeOut = setTimeout(() => {
-				modalDispatch({ type: "OPEN", payload: { render: <Login />, title: "LogIn" } });
-			}, 1000);
-		}
-		if (!!error) setErrors(error.message);
-	};
+	const handleChangeFieldValues = (newValues) => {
+		setRegisterFormFields(fields => ({ ...fields, ...newValues }));
+	}
 
 	return (
-		<div className={`simple-form-container register-form`}>
-			<SimpleForm
-				submitButtonName="Register"
-				validationSchema={validationSchema}
-				handleSubmit={handleSubmit}
-				fields={fields}
-			/>
+		<div className="register-form">
+			{registerStage === 1 &&
+				<RegisterStage1
+					changeFieldVals={handleChangeFieldValues}
+					initialFieldValues={registerFormField}
+					changeStage={setRegisterStage}
+				/>}
+			{registerStage === 2 &&
+				<RegisterStage2
+					changeStage={setRegisterStage}
+					initialFieldValues={registerFormField}
+				/>}
 		</div>
 	);
 };
---
+++ client/src/modalForms/Register/RegisterStage2.js
@@ -0,0 +1,89 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import * as Yup from "yup";
+import { Field, Form, Formik } from "formik";
+import TextInput from "components/TextInput/TextInput";
+import Button from "components/Button/Button";
+import fetchData from "helper/fetchData";
+import { ModalContext } from "context/ModalContext";
+import LoadingOverlay from "components/LoadingOverlay/LoadingOverlay";
+
+
+const validationSchema = Yup.object({
+    email: Yup.string().email().required("field is required"),
+    username: Yup.string().max(25, "username is too long").required("field is required"),
+    password: Yup.string().min(5, "must be at least 5 characters").required("field is required"),
+    matchPassword: Yup.string()
+        .oneOf([Yup.ref("password")], "password does not match")
+        .required("Password confirm is required"),
+});
+
+const fieldTypes = {
+    email: "email",
+    username: "text",
+    password: "password",
+    matchPassword: "password"
+}
+
+const RegisterStage2 = ({ initialFieldValues, changeStage }) => {
+
+    const [, modalDispatch] = useContext(ModalContext);
+
+    const handleGoBackStage = () => {
+        changeStage(stage => stage - 1);
+    }
+
+    const createFields = () => {
+        let fieldVals = {};
+        Object.keys(fieldTypes).forEach(fieldName => fieldVals[fieldName] = initialFieldValues[fieldName])
+        return fieldVals;
+    }
+
+
+    const submitRegistrationForm = async (submittedData, { setErrors, setSubmitting }) => {
+        const { data, error } = await fetchData({
+            method: "POST",
+            url: "/register",
+            setLoading: setSubmitting,
+            payload: {...initialFieldValues, ...submittedData},
+        });
+        if (!!data) modalDispatch({ type: "CLOSE" });
+        else if (!!error) setErrors(error.message);
+    }
+
+    return (
+        <Formik
+            validationSchema={validationSchema}
+            initialValues={createFields()}
+            onSubmit={submitRegistrationForm}
+        >
+            {({isSubmitting, isValid, errors }) => (
+                <>
+                    <LoadingOverlay show={!isSubmitting} opacity={0.5} />
+                    <Form>
+                        {
+                            Object.entries(fieldTypes).map(([fieldName, fieldType]) =>
+                                <Field
+                                    key={fieldName}
+                                    hasErrors={!!errors[fieldName]}
+                                    helperText={errors[fieldName]}
+                                    label={fieldName}
+                                    name={fieldName}
+                                    type={fieldType}
+                                    as={TextInput} />)
+                        }
+                        <Button classes={["register-stage-controll-btn stage-back"]} clicked={handleGoBackStage} >Go back</Button>
+                        <Button classes={["btn-accent"]} disabled={!isValid} type="submit">Finish</Button>
+                    </Form>
+                </>
+            )}
+        </Formik>
+    )
+}
+
+RegisterStage2.propTypes = {
+    initialFieldValues: PropTypes.object.isRequired,
+    changeStage: PropTypes.func.isRequired
+}
+
+export default RegisterStage2;
