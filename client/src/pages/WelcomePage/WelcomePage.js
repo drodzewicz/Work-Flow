b4592cccde780e40e9444020e5b6962616a15302
DaRoBB <drodzewicz@gmail.com>
2020-11-01 23:20:39 +0100

    redone css for welcomePage compoenent, created alternative dark mode svg images like WorFlow_person and ColumnTask

--- client/src/pages/WelcomePage/WelcomePage.js
+++ client/src/pages/WelcomePage/WelcomePage.js
@@ -2,37 +2,34 @@ import React, { useContext } from "react";
 import "./WelcomePage.scss";
 import Button from "components/Button/Button";
 import { ModalContext } from "context/ModalContext";
+import { UserContext } from "context/UserContext";
 import workflowPerson from "assets/images/workflow-person.svg";
+import workflowPersonDark from "assets/images/workflow-person_dark.svg";
 import { Register } from "modalForms";
-import ContainerBox from "components/ContainerBox/ContainerBox";
 import taskColumns from "assets/images/task_columns.svg";
+import taskColumnsDark from "assets/images/task_columns_dark.svg";
 
 const WelcomePage = () => {
 	const [, modalDispatch] = useContext(ModalContext);
+	const [{theme}] = useContext(UserContext);
 
 	const openRegisterModal = () => {
 		modalDispatch({ type: "OPEN", payload: { render: <Register />, title: "Register" } });
 	};
 
 	return (
-		<ContainerBox classes={["welcome-section"]}>
+		<div className="welcome-section">
 			<section className="introduction-section">
-				<img src={workflowPerson} alt="person-task" id="person-image" />
+				<img src={workflowPerson} alt="person-task" className={`person-image ${theme ? "" : "hide"}`} />
+				<img src={workflowPersonDark} alt="person-task" className={`person-image ${theme ? "hide" : ""}`} />
 				<div className="text-subsection">
-					<div className="main-text">
-						<h1 className="home-page-title">Work-Flow</h1>
-						<h2>work more collaboratively and get more done.</h2>
-					</div>
-					{/* <p className="home-page-description">
-					Work-Flow is a web-based Kanban-style list making application used to manage work at personal or organizational level.
-					Using this application organizing your team will be easy.
-				</p> */}
-					<div className="call-to-action home-page-description">
-						<p className="call-toaction-text">start organizing now</p>
-						<Button clicked={openRegisterModal} classes={["btn-accent"]}>
-							Join Now
+					<h1>Work-Flow</h1>
+					<h2>work more collaboratively and get more done.</h2>
+					<p>Work-Flow is a web-based Kanban-style list making application used to manage work at personal or organizational level.
+						 Using this application organizing your work will be easy.</p>
+					<Button clicked={openRegisterModal} classes={["btn-accent join-now"]}>
+						Join us Now
 					</Button>
-					</div>
 				</div>
 			</section>
 			<section className="task-column-section">
@@ -40,10 +37,11 @@ const WelcomePage = () => {
 					<h1>Tasks</h1>
 					<h2>Create tasks and assign them to your teammates - add custom tags to differentiate tasks at a glance</h2>
 				</div>
-				<img src={taskColumns} alt="task-columns" className="task-columns-img" />
+				<img src={taskColumns} alt="task-columns" className={`task-columns-img ${theme ? "" : "hide"}`} />
+				<img src={taskColumnsDark} alt="task-columns" className={`task-columns-img ${theme ? "hide" : ""}`} />
 			</section>
 
-		</ContainerBox>
+		</div>
 	);
 };
 
