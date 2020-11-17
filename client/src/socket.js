9b9d78dd927f57a4ae31eeaaa745c007502e9f8e
DaRoBB <drodzewicz@gmail.com>
2020-11-17 12:55:31 +0100

    console logs in client

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,9 +2,6 @@ import io from "socket.io-client";
 
 const BASE_API_URI = process.env.REACT_APP_API_URI || "http://localhost:8080"
 const API_URI = process.env.NODE_ENV === "production" ? "/" : BASE_API_URI
-console.log(process.env.NODE_ENV)
-console.log(BASE_API_URI)
-console.log(API_URI)
 const ws = io.connect(API_URI)
 
 export { ws }
