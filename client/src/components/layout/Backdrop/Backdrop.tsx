e977236fc11e58ebe9f4b9c419103c1c982c2a29
Darek <DRodzewicz@gmail.com>
2023-12-02 13:10:34 +0100

    Added Modal tests

--- client/src/components/layout/Backdrop/Backdrop.tsx
+++ client/src/components/layout/Backdrop/Backdrop.tsx
@@ -12,6 +12,7 @@ const Backdrop: React.FC<BackdropProps> = ({ show, onClick, opacity = 0.6 }) => 
   <div
     onClick={onClick}
     role="presentation"
+    aria-label="backdrop"
     style={{ opacity: show ? opacity : 0 }}
     className="backdrop"
   ></div>
--- client/src/components/layout/Modal/Modal.tsx
+++ client/src/components/layout/Modal/Modal.tsx
@@ -13,14 +13,14 @@ export type IModalProps = {
   title: string;
   scrollable?: boolean;
   onClose: () => void;
-  size: "s" | "m" | "l";
+  size?: "s" | "m" | "l";
 };
 
 const Modal: React.FC<React.PropsWithChildren<IModalProps>> = ({
   show,
   title,
   children,
-  size,
+  size = "m",
   scrollable = false,
   onClose,
 }) => {
@@ -29,16 +29,17 @@ const Modal: React.FC<React.PropsWithChildren<IModalProps>> = ({
       <Portal mountTo="root-modal">
         <>
           <div className="modal-wrapper">
-            <aside className={`modal modal_size--${size}`}>
+            <aside role="dialog" className={`modal modal_size--${size}`}>
               <header className="modal__header">
                 <h2 className="modal__header__title">{title}</h2>
-                <FaTimes
-                  role="button"
-                  aria-label="Close"
-                  tabIndex={0}
+                <button
+                  aria-label="close"
                   onClick={onClose}
+                  tabIndex={0}
                   className="modal__header__close"
-                />
+                >
+                  <FaTimes />
+                </button>
               </header>
               <section
                 className={`modal__content scrollbar ${
