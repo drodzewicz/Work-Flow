03c72d2f24fc18626354a6b4323c1b811b5e526a
Darek <DRodzewicz@gmail.com>
2023-12-26 23:34:50 +0200

    added comments to useWindowSize

--- client/src/hooks/useWindowSize/useWindowSize.ts
+++ client/src/hooks/useWindowSize/useWindowSize.ts
@@ -3,12 +3,14 @@ import { useLayoutEffect, useState } from "react";
 const useWindowSize = () => {
   const [size, setSize] = useState([0, 0]);
   useLayoutEffect(() => {
-    function updateSize() {
+    function setWindowSize() {
       setSize([window.innerWidth, window.innerHeight]);
     }
-    window.addEventListener("resize", updateSize);
-    updateSize();
-    return () => window.removeEventListener("resize", updateSize);
+    // initial call of the function to set window size
+    setWindowSize();
+    // add function to event listener
+    window.addEventListener("resize", setWindowSize);
+    return () => window.removeEventListener("resize", setWindowSize);
   }, []);
   return size;
 };
