e8994f8100c4b7522cd3b3a5799679def65cc678
Darek <DRodzewicz@gmail.com>
2024-04-27 01:26:25 +0200

    Restyled role table

--- client/src/components/general/RoleTable/RoleTable.tsx
+++ client/src/components/general/RoleTable/RoleTable.tsx
@@ -1,57 +1,89 @@
 import React, { useMemo } from "react";
 
 import { getRoleIcon } from "@/utils/role";
-import { IconType } from "react-icons";
 
 import "./RoleTable.scss";
-
-import TableCheckboxCell from "./TableCheckboxCell";
-import TableHeader from "./TableHeader";
+import { Permissions } from "@/hooks/useRBAC";
+import { FaCheck, FaTimes } from "react-icons/fa";
 
 type RoleTableProps = {
     permissions: string[];
     roles: Record<string, { permissions: string[] }>;
 };
 
-const RoleTable: React.FC<RoleTableProps> = ({ permissions, roles }) => {
-    const data: { role: string; RoleIcon: IconType; rolePermissions: Record<string, boolean> }[] =
-        useMemo(() => {
-            return Object.keys(roles).map((role) => {
-                const rolePermissions = permissions.reduce(
-                    (acc, permission) => ({
-                        ...acc,
-                        [`${permission}`]: roles[role].permissions.includes(permission),
-                    }),
-                    {}
-                );
-                return { role, RoleIcon: getRoleIcon(role), rolePermissions };
-            });
-        }, [roles, permissions]);
+const RoleTable: React.FC<RoleTableProps> = ({ roles }) => {
+    const roleNames = useMemo(() => Object.keys(roles), [roles]);
+
+    const groupedPemrissions: Record<string, Record<string, string>> = {
+        Board: {
+            [Permissions.BOARD_DELETE]: "Delete",
+            [Permissions.BOARD_UPDATE]: "Update",
+        },
+        Task: {
+            [Permissions.TASK_CREATE]: "Create",
+            [Permissions.TASK_DELETE]: "Delete",
+            [Permissions.TASK_MOVE]: "Move",
+        },
+        Column: {
+            [Permissions.COLUMN_CREATE]: "Create",
+            [Permissions.COLUMN_DELETE]: "Delete",
+            [Permissions.COLUMN_MOVE]: "Move",
+        },
+        Tag: {
+            [Permissions.TAG_CREATE]: "Create",
+            [Permissions.TAG_DELETE]: "Delete",
+        },
+        Members: {
+            [Permissions.MEMBER_ADD]: "Add",
+            [Permissions.MEMBER_DELETE]: "Delete",
+            [Permissions.MEMBER_ROLE_UPDATE]: "Role update",
+        },
+    };
 
     return (
-        <div className="table-wrapper scrollbar">
-            <table className="role-table">
-                <TableHeader columns={permissions} />
-                <tbody>
-                    {data.map(({ role, RoleIcon, rolePermissions }) => (
-                        <tr key={`${role}-row`}>
-                            <td className="role-table__first-col">
-                                <div className="role-table__role">
-                                    <RoleIcon />
-                                    {role}
+        <div className="role-table">
+            <div className="role-table__column">
+                <div className="role-table__header">Role</div>
+                {roleNames.map((role) => {
+                    const Icons = getRoleIcon(role);
+                    return (
+                        <div key={`role-column-${role}`} className="role-table__row">
+                            <Icons />
+                            {role}
+                        </div>
+                    );
+                })}
+            </div>
+            <div className="role-table__body">
+                {Object.entries(groupedPemrissions).map(([group, permissions]) => (
+                    <div key={group} className="role-table__column">
+                        <div className="role-table__header">{group}</div>
+                        <div className="role-table__grouped-column">
+                            {Object.entries(permissions).map(([permission, label]) => (
+                                <div key={`permission-column-${permission}`}>
+                                    <div className="role-table__header">{label}</div>
+                                    {roleNames.map((role) => (
+                                        <div
+                                            key={`role-permission-cell-${role}`}
+                                            className="role-table__row"
+                                        >
+                                            {roles[role].permissions.includes(permission) ? (
+                                                <div className="role-circle role-circle--active">
+                                                    <FaCheck />
+                                                </div>
+                                            ) : (
+                                                <div className="role-circle role-circle--inactive">
+                                                    <FaTimes />
+                                                </div>
+                                            )}
+                                        </div>
+                                    ))}
                                 </div>
-                            </td>
-                            {permissions.map((permission) => (
-                                <TableCheckboxCell
-                                    key={`${permission}-cell`}
-                                    hasPermission={rolePermissions[permission]}
-                                />
                             ))}
-                            <td className="role-table__last-col"></td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
